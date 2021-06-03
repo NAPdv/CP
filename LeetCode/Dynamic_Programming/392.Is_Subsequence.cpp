b045c66b0ac8c067d2b849a22c6ddf04e716f4a2
Abhishek Kumar Singh <43106034+NAPdv@users.noreply.github.com>
2021-06-03 19:25:56 +0530

    Create 392.Is_Subsequence.cpp

---
+++ LeetCode/Dynamic_Programming/392.Is_Subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        int i=0,j=0;
+        int c=0;
+        while(i<n&&j<m)
+        {
+            if(s[i]==t[j])
+            {
+                i++;
+                j++;
+                c++;
+            }
+            else
+            {
+                j++;
+            }
+                
+        }
+        return c==n;
+    }
+};
