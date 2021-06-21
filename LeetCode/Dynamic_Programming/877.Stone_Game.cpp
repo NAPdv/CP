5f903dfa43468add2bf9fbf7232fd5633c2e0aaa
Abhishek Kumar Singh <43106034+NAPdv@users.noreply.github.com>
2021-06-21 19:55:25 +0530

    Update 877.Stone_Game.cpp

--- LeetCode/Dynamic_Programming/877.Stone_Game.cpp
+++ LeetCode/Dynamic_Programming/877.Stone_Game.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int dp[502];
+    int dp[502][502];
     
     int gt(vector<int>&a,int i,int j)
     {
-        if(dp[i]!=-1) return dp[i];
+        if(dp[i][j]!=-1) return dp[i][j];
         if(i+1==j)
         {
-            return dp[i] = max(a[i],a[j]);
+            return dp[i][j] = max(a[i],a[j]);
         }
-        return dp[i] = max(a[i]+min(gt(a,i+2,j),gt(a,i+1,j-1)),a[j]+min(gt(a,i,j-2),gt(a,i+1,j-1)));
+        return dp[i][j] = max(a[i]+min(gt(a,i+2,j),gt(a,i+1,j-1)),a[j]+min(gt(a,i,j-2),gt(a,i+1,j-1)));
     }
     bool stoneGame(vector<int>& piles) {
         int sum=0;
